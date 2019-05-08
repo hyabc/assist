eccc8f34c8880240b168816f57b0d55608020403
hyabc <hyabc0@gmail.com>
2019-05-08 09:55:14 +0800

    Default Update Message

--- dist.py
+++ dist.py
@@ -1,13 +1,11 @@
 import serial
 import time
 import socket
-#ser = serial.Serial('/dev/ttyACM0', 115200)
+ser = serial.Serial('/dev/ttyACM0', 115200)
 address = ('127.0.0.1', 8888)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#ser.open()
 while 1:
-#response = ser.readline()
-	response = bytes("Hello, World!", 'utf-8')
+	response = ser.readline()
+	print(response)
 	sock.sendto(response, address)
-	time.sleep(1)
-#s.close()
+ser.close()

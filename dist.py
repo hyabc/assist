1a014450e6733c12a67aba722706c83adad6fbf0
hyabc <hyabc0@gmail.com>
2019-05-10 14:49:09 +0800

    Default Update Message

--- dist.py
+++ dist.py
@@ -4,8 +4,11 @@ import socket
 ser = serial.Serial('/dev/ttyACM0', 115200)
 address = ('127.0.0.1', 8888)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+cnt = 0
 while 1:
 	response = ser.readline()
-	print(response)
-	sock.sendto(response, address)
+	if cnt > 20:
+		print(response)
+		sock.sendto(response, address)
+	cnt += 1
 ser.close()

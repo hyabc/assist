6b585a408d6b22b1cb0523ea16f91b3cf0dbdd00
hyabc <hyabc0@gmail.com>
2019-07-22 10:07:40 +0800

    Add servo support on Arduino

--- laser/laser.ino
+++ laser/laser.ino
@@ -1,16 +1,31 @@
 #include "Adafruit_VL53L0X.h"
+#include <Servo.h>
 Adafruit_VL53L0X sensor = Adafruit_VL53L0X();
+VL53L0X_RangingMeasurementData_t dist;
+Servo servo;
+int angle;
+#define MAXANGLE 150
 void setup() {
 	Serial.begin(115200);
 	while (!Serial) delay(1);
-  sensor.begin();
+	sensor.begin();
+	servo.attach(11);
 }
 void loop() {
-	VL53L0X_RangingMeasurementData_t dist;
-	sensor.rangingTest(&dist, false);
-	if (dist.RangeStatus != 4)
-		Serial.println(dist.RangeMilliMeter);
-	else
-		Serial.println(-1);
-	delay(10);
+	for (angle = 90;angle <= MAXANGLE;angle += 5) {
+		servo.write(angle);
+		delay(20);
+		sensor.rangingTest(&dist, false);
+		if (dist.RangeStatus != 4)
+			Serial.print(dist.RangeMilliMeter);
+		else
+			Serial.print(-1);
+		Serial.print(' ');
+	}
+	for (angle = MAXANGLE;angle >= 90;angle -= 5) {
+		servo.write(angle);
+		delay(10);
+	}
+	delay(50);
+	Serial.println();
 }

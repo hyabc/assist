b0752d84cb3002a996449e1f6dccb0eec6da31c2
hyabc <hyabc0@gmail.com>
2019-07-26 17:29:28 +0800

    Bugfix

--- spawn.c
+++ spawn.c
@@ -5,13 +5,18 @@
 #include <stddef.h> 
 #include <string.h>
 int main() {
-	if (!fork()) execl("speech", "speech", NULL);
+	if (!fork()) execlp("python3", "python3", "launch_speech.py", NULL);
+
 	sleep(1);
+
 	if (!fork()) execl("proxy", "proxy", NULL);
+
 	sleep(1);
+
 	if (!fork()) execlp("python3", "python3", "adapter1.py", NULL);
 	if (!fork()) execlp("python3", "python3", "adapter2.py", NULL);
 	if (!fork()) execl("position", "position", NULL);
 	if (!fork()) execl("voltage", "voltage", NULL);
+
 	return 0;
 }
--- vision.cpp
+++ vision.cpp
@@ -1,19 +1,25 @@
-#include <cstdio>
+extern "C" {
 #include "darknet.h"
+}
+#include <cstdio>
 #include <ctime>
 #include <cstdlib>
 #include <opencv2/opencv.hpp>
+extern "C" {image mat_to_image(cv::Mat);}
+
 #define thresh 0.5
 #define hier_thresh 0.5
 #define nms_thresh 0.4
 #define max(x, y) ({x > y ? x : y;})
 #define min(x, y) ({x < y ? x : y;})
+
 char buffer[100];
-extern "C" {image mat_to_image(cv::Mat);}
 char buf[100];
+
 namespace traffic_light {
 	double area;
 	int cur_option;
+
 	void judge(cv::Mat src, int option) {
 		cv::Mat mid, mask1, mask2, obj;
 		cv::cvtColor(src, mid, cv::COLOR_BGR2HSV);
@@ -23,12 +29,17 @@ namespace traffic_light {
 			cv::bitwise_or(mask1, mask2, obj);
 		} else
 			cv::inRange(mid, cv::Scalar(60, 64, 100), cv::Scalar(90, 255, 255), obj);
+
 		std::vector<std::vector<cv::Point> > contours;
 		cv::findContours(obj, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+
 		if (contours.size() == 0) return;
+
 		int cur = 0;
 		for (int i = 0;i < contours.size();i++) if (cv::contourArea(contours[i]) > cv::contourArea(contours[cur])) cur = i;
+
 		if (cv::contourArea(contours[cur]) < 40) return;
+
 		cv::Rect rcur = cv::boundingRect(cv::Mat(contours[cur]));
 		double x = (double)(rcur.height) / rcur.width;
 		if (x < 1.5) return;
@@ -49,12 +60,12 @@ int main() {
 	network *net = load_network("yolov3.cfg", "yolov3.weights", 0);
 	set_batch_network(net, 1);
 	layer l = net->layers[net->n-1];
+
 	for (int iter = 1;iter <= 1;iter++) {
 		traffic_light::cur_option = -1;
 
-
 		printf("BEGIN %d\n", iter);
-		time_t start = clock();
+//		time_t start = clock();
 		cv::Mat f;
 		//cap >> f;
 		f = cv::imread("a.jpg");
@@ -66,6 +77,7 @@ int main() {
 		detection *dets = get_network_boxes(net, frame.w, frame.h, thresh, hier_thresh, 0, 1, &count);
 		do_nms_sort(dets, count, l.classes, nms_thresh);
 		draw_detections(frame, dets, count, thresh, names, alphabet, l.classes);
+
 		for (int i = 0;i < count;i++) {
 			int cur = -1;
 			double maxprob = 0.0;
@@ -107,7 +119,7 @@ int main() {
 		save_image(frame, buf);
 		free_image(frame);
 		free_image(sized);
-		printf("%f\n", (double)(clock() - start) / CLOCKS_PER_SEC);
+//		printf("%f\n", (double)(clock() - start) / CLOCKS_PER_SEC);
 
 		if (traffic_light::cur_option != -1) 
 			printf("Color: %s\n", traffic_light::cur_option == 0 ? "Red" : "Green");
